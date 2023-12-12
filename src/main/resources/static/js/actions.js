4259946074b2fd376319ff3282604bcb37b3d142
Richmond <130723287+richmondpangan@users.noreply.github.com>
2023-12-12 18:38:31 +0800

    Modified actions JS
    - Created a dedicated function saveAccount() to handle AJAX requests for saving accounts
    - Used jQuery AJAX for asynchronous communication with the server
    - Updated success handling to close the addNewAccount modal, refresh the UI, and log success
    - Implemented error handling to log and display an error message in case of failures

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -8,3 +8,37 @@ const sidebarToggler = document.querySelector(".sidebar-collapse-btn");
 sidebarToggler.addEventListener("click", function() {
     document.querySelector("#sidebar.collapsed").classList.remove("collapsed");
 });
+
+
+function saveAccount() {
+    // Collect data from the form
+    let formData = {
+        accountType: $('#accountTypeInput').val(),
+        accountName: $('#accountNameInput').val(),
+        balance: $('#accountBalanceInput').val()
+    };
+    
+    // User ID
+    let userId = 1;
+
+    // Make an AJAX request to save the account
+    $.ajax({
+        type: 'POST',
+        url: '/users/' + userId + '/accounts', // controller endpoint
+        data: JSON.stringify(formData), // If formData is a JSON object
+        contentType: 'application/json',
+        success: function (response) {
+            // Handle success (e.g., close the modal, update UI)
+            $('#addNewAccount').modal('hide');
+            
+            // Reload the page to reflect the updated account list
+            location.reload();
+            
+            console.log('Success', response);
+        },
+        error: function (error) {
+            // Handle error (e.g., show an error message)
+            console.error('Error saving account:', error);
+        }
+    });
+}
