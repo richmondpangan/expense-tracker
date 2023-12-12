8a388cb362dc96726bf4eca4712616c3fe778a5b
Richmond <130723287+richmondpangan@users.noreply.github.com>
2023-12-12 18:37:10 +0800

    Modified AccountsViewController class
    - Autowired UserService for data access
    - Implemented addAccount endpoint to add new account

--- src/main/java/com/project/expensetracker/controller/AccountsViewController.java
+++ src/main/java/com/project/expensetracker/controller/AccountsViewController.java
@@ -5,13 +5,19 @@
 package com.project.expensetracker.controller;
 
 import com.project.expensetracker.model.Accounts;
+import com.project.expensetracker.model.User;
 import com.project.expensetracker.service.AccountsService;
+import com.project.expensetracker.service.UserService;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class AccountsViewController {
@@ -19,10 +25,25 @@ public class AccountsViewController {
     @Autowired
     private AccountsService accountsService;
     
+    @Autowired
+    private UserService userService;
+    
     @GetMapping("/users/{userId}/accounts")
     public String getAllAccounts(@PathVariable Integer userId, Model model) {
         List<Accounts> accounts = accountsService.getAllAccounts(userId);
         model.addAttribute("accounts", accounts);
         return "pages/accounts";
+    }
+    
+    @PostMapping("/users/{userId}/accounts")
+    public ResponseEntity<String> addAccount(@RequestBody @Valid Accounts accounts, @PathVariable Integer userId) {
+        // Fetch the existing user from the database
+        User existingUser = userService.getUser(userId);
+
+        // Set the existing user as the user for the account
+        accounts.setUser(existingUser);
+
+        accountsService.addAccount(accounts);
+        return ResponseEntity.ok("Account saved successfully");
     }
 }
