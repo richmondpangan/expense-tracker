83a1c3e25df3b13d34aa8f765bd14e2ca58de790
Richmond <130723287+richmondpangan@users.noreply.github.com>
2023-12-16 21:23:33 +0800

    Modified actions JS
    - Moved the variable that holds a temporary userId for testing outside the saveAccount() function
    - Modified AJAX url of saveAccount() function by adding /add
    - Modified AJAX success by removing location.reload() and adding a function to show successModal
    - Created a dedicated function addedSuccessfully() to fetch the updated accounts table after adding a new account
    - Created a dedicated function openAddAccountModal() to open the Add New Account Modal

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -9,6 +9,9 @@ sidebarToggler.addEventListener("click", function() {
     document.querySelector("#sidebar.collapsed").classList.remove("collapsed");
 });
 
+    
+// User ID
+let userId = 1;
 
 function saveAccount() {
     // Collect data from the form
@@ -17,28 +20,93 @@ function saveAccount() {
         accountName: $('#accountNameInput').val(),
         balance: $('#accountBalanceInput').val()
     };
-    
-    // User ID
-    let userId = 1;
 
     // Make an AJAX request to save the account
     $.ajax({
         type: 'POST',
-        url: '/users/' + userId + '/accounts', // controller endpoint
+        url: '/users/' + userId + '/accounts/add', // controller endpoint
         data: JSON.stringify(formData), // If formData is a JSON object
         contentType: 'application/json',
         success: function (response) {
-            // Handle success (e.g., close the modal, update UI)
-            $('#addNewAccount').modal('hide');
+            // Hide Add New Account modal after successfully adding a new account
+            $('#addAccountModal').modal('hide');
+            console.log(response);
             
-            // Reload the page to reflect the updated account list
-            location.reload();
+            //updateAccountsTable();
             
-            console.log('Success', response);
+            $('#successModal').modal('show');
         },
         error: function (error) {
             // Handle error (e.g., show an error message)
-            console.error('Error saving account:', error);
+            console.error('Error adding a new account:', error);
         }
     });
 }
+
+function addedSuccessfully() {        
+    $.ajax({
+       type: 'GET',
+       url: '/users/' + userId + '/accounts/fetchUpdate',
+       dataType: 'json',
+       success: function (data) {
+            // Clear the existing table body
+            let tableBody = $('#accountsTableBody');
+            tableBody.empty();
+            
+            for (let i = 0; i < data.length; i++) {
+                let account = data[i];
+                tableBody.append('<tr>'
+                        + '<td>' + account.accountType + '</td>'
+                        + '<td>' + account.accountName + '</td>'
+                        + '<td>' + account.balance + '</td>'
+                        + '<td>' + '<button class="btn btn-success" type="button">Edit</button>' + '</td>'
+                        + '<td>' + '<button class="btn btn-danger" type="button">Delete</button>' + '</td>'
+                       + '</tr>');
+            }
+            console.log('addedSuccessfully Modal working');
+       },
+       error: function (error) {
+           console.log('Error: ', error);
+       }
+    });
+    // Hide sucess modal after clicking the 'okay' button
+    $('#successModal').modal('hide');
+    
+    // Reload the page to reflect the updated account list
+    //location.reload();
+}
+
+function openAddAccountModal() {
+    $.ajax({
+       type: 'GET',
+       url: '/users/' + userId + '/accounts/add',
+       success: function () {
+           $('#addAccountModal').modal('show');
+           console.log('openAddAccountModal working');
+       },
+       error: function (error) {
+           console.log('Error:', error);
+       }
+    });
+}
+
+//function updateAccountsTable() {
+//    let tableBody = $('#accountsTableBody');
+//    
+//    // Clear the existing table body
+//    tableBody.empty();
+//    
+//    // Iterate over the accounts and append rows to the table
+//    $.each(accounts, function(_, account) {
+//        let row = $('<tr>');
+//        row.append('<td>' + account.accountType + '</td>');
+//        row.append('<td>' + account.accountName + '</td>');
+//        row.append('<td>' + account.balance + '</td>');
+//        row.append('<td>' +
+//            '<button class="btn btn-success" type="button">Edit</button>' +
+//            '<button class="btn btn-danger" type="button">Delete</button>' +
+//            '</td>');
+//
+//        tableBody.append(row);
+//    });
+//}
